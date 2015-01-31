6f037a4ff2cb51002e29fab3f40c7b5b8a504fa6
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-01-31 15:53:52 +0100

    Update #7

--- Villageois/WebFolder/index.waPage/scripts/index-tablet.js
+++ Villageois/WebFolder/index.waPage/scripts/index-tablet.js
@@ -2,19 +2,38 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var image3 = {};	// @image
+	var image2 = {};	// @image
 	var documentEvent = {};	// @document
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	image3.click = function image3_click (event)// @startlock
+	{// @endlock
+		chgLangue("en");
+	};// @lock
+
+	image2.click = function image2_click (event)// @startlock
+	{// @endlock
+		chgLangue("fr");
+	};// @lock
+
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		
-		localization.changeLanguage("fr"); 
+		chgLangue("fr"); 
 		
 	};// @lock
 
+	function chgLangue(lg){
+		
+		localization.changeLanguage(lg); 
+		
+	};
 // @region eventManager// @startlock
+	WAF.addListener("image3", "click", image3.click, "WAF");
+	WAF.addListener("image2", "click", image2.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 // @endregion
 };// @endlock
